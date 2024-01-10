a3b506d5f593590a36a1a5758c60145b1c3679b7
Sachin Vaid <vaid.mca@gmail.com>
2024-01-09 22:14:13 -0500

    Added Microservices project using ocelot

--- Project.API/obj/Debug/net7.0/Project.API.AssemblyInfo.cs
+++ Project.API/obj/Debug/net7.0/Project.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd3b4849da3566c7419023941adeb693837ee66f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
